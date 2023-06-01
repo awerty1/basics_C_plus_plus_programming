1a45e50db35f9e550cd306075693182509f60e68
pheno <testuniversity@yandex.ru>
2023-06-02 01:54:36 +0300

    Update 4_2.cpp
    refactoring and fixes

--- 9 variant/4_2.cpp
+++ 9 variant/4_2.cpp
@@ -7,55 +7,80 @@
 
 #include <iostream>
 #include <ostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    cout << "Введите число n больше 0";
-    int n = 0, m = 0, sum = 0, min = 0;
+    
+    cout << "Введите число n больше 0:";
+    int n = 0; 
+    int m = 0; 
+    int sum = 0; 
+    int min = 0;
+    
     srand(time(NULL));
+    
     do
     {
         cin >> n;
-        if(n<=0)
-            cout << "Число n должно быть больше 0";
-    }while(n<=0);
-    n--;
-    cout << "Введите число m больше 0";
+        if(n <= 0)
+            cout << "Число n должно быть больше 0:";
+    }
+    while(n <= 0);
+    
+    cout << "Введите число m больше 0:";
     do
     {
         cin >> m;
-        if(m<=0)
-            cout << "Число m должно быть больше 0";
-    }while(m<=0);
-    m--;
+        if(m <= 0)
+            cout << "Число m должно быть больше 0:";
+    }
+    while(m <= 0);
     
-    int** array = new int*[m];
-    for(int i = 0; i<=m;i++)
-        array[i]=new int[n];
-        
-    for(int i = 0; i<=m;i++)
+    int** array = new int*[n];
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j<=n;j++)
+        array[i]=new int[m];
+    }
+        
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
         {
-            array[i][j] = rand() % 9+1;
-            cout << array[i][j] << "\t";
-            sum = sum + array[i][j];
+            array[i][j] = rand() % 10+1;
+            cout << setw(4) << array[i][j];
+            sum += array[i][j];
         }
-        if(i == 1) min = sum;
-        if(i % 2 > 0)
+        
+        if(i == 1)
         {
-            if(sum < min)
+            min = sum;
+        }
+        
+        if(i % 2 != 0)
+        {
+            if(sum < min && min != 0)
             {
                 min = sum;
             }
-            cout << "Сумма элементов = " << sum << "\n";
+            cout << " Сумма элементов = " << sum << "\n";
         }
         else
+        {
             cout << "\n";
+        }
         sum = 0;
     }
     cout << "Наименьшая сумма элементов четных строк = " << min << "\n";
+	
+	for(int i = 0; i < n ; i++)
+	{
+		delete[] array[i];
+	}
+    delete[] array;
+	
+	return 0;
 }
