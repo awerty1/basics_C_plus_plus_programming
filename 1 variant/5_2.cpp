f79e7370c35a2d00182c74412c9ce0ab21d3bfe3
pheno <testuniversity@yandex.ru>
2023-07-16 00:08:11 +0300

    Update 5_2.cpp
    added improvements

--- 1 variant/5_2.cpp
+++ 1 variant/5_2.cpp
@@ -8,6 +8,11 @@
 
 std::string doubleSpaces(const std::string& inputString) 
 {
+	if (inputString.empty()) {
+        // Если введенная строка пустая, возвращаем пустую строку
+        return "String is empty!";
+    }
+	
     std::string result;
 
     // Проходим по каждому символу в исходной строке
@@ -36,7 +41,15 @@ int main()
     std::getline(std::cin, inputString);
 
     std::string transformedString = doubleSpaces(inputString);
-    std::cout << "Преобразованная строка: " << transformedString << std::endl;
+	if (transformedString != "String is empty!")
+	{
+		std::cout << "Преобразованная строка: " << transformedString << std::endl;
+	}
+	else
+	{
+		std::cout << transformedString;
+	}
+		
 
     return 0;
 }
