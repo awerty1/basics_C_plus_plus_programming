fd16515703202f4a9580a020161a305ab102186b
pheno <testuniversity@yandex.ru>
2023-10-26 00:00:31 +0300

    Create 1.4.cpp
    added lab 1.4

---
+++ 12 variant/1.4.cpp
@@ -0,0 +1,33 @@
+/*
+Задание 4. Дано действительное число A. Для функции F(X), график 
+которой представлен на рисунке, вычислить F(A) и вывести на экран.
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main() 
+{
+    double A;
+    std::cout << "Введите значение числа A: ";
+    std::cin >> A;
+    
+    double result;
+    
+    if (A < 0) 
+	{
+        result = -A;
+    } 
+	else if (A >= 0 && A <= 4) 
+	{
+        result = std::sqrt(A);
+    } 
+	else 
+	{
+        result = 2;
+    }
+    
+    std::cout << "Значение функции F(A) = " << result << std::endl;
+    
+    return 0;
+}
