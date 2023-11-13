91cd97b3e89ace447913c136009317ace93e3acd
pheno <testuniversity@yandex.ru>
2023-11-13 09:02:03 +0300

    Create lab1.1.cpp
    added lab 1.1

---
+++ 3 variant/lab1.1.cpp
@@ -0,0 +1,52 @@
+/*
+ * Задание 1. Написать программу вычисления объема параллелепипеда. 
+ * Исходные данные вводятся с клавиатуры.
+*/
+
+#include <iostream>
+
+double calculate_volume(double length, double width, double height)
+{
+	if (length <= 0 || width <= 0 || height <= 0)
+	{
+		throw std::runtime_error("Все параметры параллелепипеда должны быть положительными числами.");
+	}
+	return length * width * height;
+}
+
+int main() 
+{
+    double length, width, height;
+    bool isValidInput = false;
+    
+    do 
+	{
+        std::cout << "Введите длину параллелепипеда: ";
+        std::cin >> length;
+
+        std::cout << "Введите ширину параллелепипеда: ";
+        std::cin >> width;
+
+        std::cout << "Введите высоту параллелепипеда: ";
+        std::cin >> height;
+        
+        try 
+        {
+            double volume = calculate_volume(length, width, height);
+            std::cout << "Объем параллелепипеда: " << volume << std::endl;
+            isValidInput = true;
+        } 
+        catch (const std::exception& ex) 
+        {
+            std::cout << "Ошибка: " << ex.what() << std::endl;
+        }
+    } 
+	while(!isValidInput);
+
+    return 0;
+}
+
+
+
+
+
