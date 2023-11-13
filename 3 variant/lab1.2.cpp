9d7270174e01ce73ccc4d57272e3fb5daf33ed08
pheno <testuniversity@yandex.ru>
2023-11-14 01:27:28 +0300

    Create lab1.2.cpp
    added lab 1.2

---
+++ 3 variant/lab1.2.cpp
@@ -0,0 +1,32 @@
+/*
+Задание 2. Написать программу пересчета величины временного интервала, 
+заданного в секундах, в величину, выраженную в часах, минутах и секундах. 
+Интервал в секундах вводится, например, 3725.Тогда программа должна вывести 
+1 ч. 2 м. 5 сек
+*/
+
+#include <iostream>
+
+int main() 
+{
+    int seconds;
+    do 
+	{
+        std::cout << "Введите интервал в секундах: ";
+        std::cin >> seconds;
+        
+        if (seconds < 0) 
+		{
+            std::cout << "Ошибка! Введите положительное число." << std::endl;
+        }
+    } 
+	while (seconds < 0);
+
+    int hours = seconds / 3600;
+    int minutes = (seconds % 3600) / 60;
+    int remainingSeconds = (seconds % 3600) % 60;
+
+    std::cout << hours << " ч. " << minutes << " м. " << remainingSeconds << " сек." << std::endl;
+
+    return 0;
+}
