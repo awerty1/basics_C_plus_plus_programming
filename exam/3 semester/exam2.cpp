ba40c781eae0e9c10a18fb525297adba2b634964
pheno <testuniversity@yandex.ru>
2023-12-16 17:31:25 +0300

    Create exam2.cpp
    added exam2.cpp

---
+++ exam/3 semester/exam2.cpp
@@ -0,0 +1,28 @@
+/*
+Вычисление делителей для заданного числа
+*/
+
+#include <iostream>
+
+int findDivisors(int n) 
+{
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			count++;
+		}
+	}
+		
+    return count;
+}
+
+int main()
+{
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+    std::cout << "Number of divisors: " << findDivisors(n);
+    return 0;
+}
