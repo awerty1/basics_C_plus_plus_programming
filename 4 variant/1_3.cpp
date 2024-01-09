5d0ea4f0a1df9e4fc417adda06b63c4e6279a976
pheno <testuniversity@yandex.ru>
2024-01-09 21:16:17 +0300

    Update 1_3.cpp
    refactored

--- 4 variant/1_3.cpp
+++ 4 variant/1_3.cpp
@@ -11,25 +11,25 @@ int main()
     int m, n;
     std::cout << "Введите два числа: ";
     std::cin >> m >> n;
-
-    if (m == n) 
+    
+    if (m == n)
     {
         // оба числа равны
         int result = m + n;
         std::cout << "Результат: " << result << std::endl;
-    } 
-    else 
+    }
+    else
     {
         // числа разные
         int smaller = (m < n) ? m : n;
         int larger = (m > n) ? m : n;
-
+        
         int halfSum = (m + n) / 2;
         int doubleProduct = 2 * m * n;
-
+        
         std::cout << "Меньшее значение: " << halfSum << std::endl;
         std::cout << "Большее значение: " << doubleProduct << std::endl;
     }
-
+    
     return 0;
 }
