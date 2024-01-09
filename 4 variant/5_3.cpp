1124796e06ac2f9222e15c9bdcbcc6e5b040edc2
pheno <testuniversity@yandex.ru>
2024-01-09 20:58:21 +0300

    Update 5_3.cpp
    refactored

--- 4 variant/5_3.cpp
+++ 4 variant/5_3.cpp
@@ -13,14 +13,14 @@ bool isPalindrome(const std::string& word)
     std::string processedWord;
     
     // Удаление знаков пунктуации и изменение регистра
-    for (char c : word) 
-	{
+    for (char c : word)
+    {
         if (std::isalpha(c)) 
-		{
+        {
             processedWord += std::tolower(c);
         }
     }
-
+    
     // Проверка на палиндром
     std::string reversedWord = processedWord;
     std::reverse(reversedWord.begin(), reversedWord.end());
@@ -33,19 +33,19 @@ int main()
     std::string word;
     std::cout << "Введите слово: ";
     std::getline(std::cin, word);
-
-    if (word.empty()) 
-	{
+    
+    if (word.empty())
+    {
         std::cout << "Ошибка: введена пустая строка." << std::endl;
         return 0; // Завершаем программу
     }
-
-    if (isPalindrome(word)) 
-	{
+    
+    if (isPalindrome(word))
+    {
         std::cout << "Данное слово является палиндромом." << std::endl;
-    } 
-	else 
-	{
+    }
+    else
+    {
         std::cout << "Данное слово не является палиндромом." << std::endl;
     }
     
