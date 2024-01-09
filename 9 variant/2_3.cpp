75e2b63b7b56c8d6e0177643edb4881f91ab3602
pheno <testuniversity@yandex.ru>
2024-01-09 20:14:09 +0300

    Update 2_3.cpp
    updated 2_3

--- 9 variant/2_3.cpp
+++ 9 variant/2_3.cpp
@@ -10,13 +10,13 @@ bool is_palindrome(int n)
 {
     int rev = 0;
     int original = n;
-
+    
     while (n > 0) 
-	{
+    {
         rev = rev * 10 + n % 10;
         n /= 10;
     }
-
+    
     return (original == rev);
 }
 
@@ -25,22 +25,22 @@ int main()
     int n;
     std::cout << "Введите число: ";
     std::cin >> n;
-
+    
     // Проверка на корректность ввода
     if (n <= 0 || n >= 1000000) 
     {
         std::cout << "Ошибка ввода" << std::endl;
         return 0;
     }
-
+    
     if (is_palindrome(n)) 
-	{
+    {
         std::cout << "yes" << std::endl;
     } 
-	else 
-	{
+    else 
+    {
         std::cout << "no" << std::endl;
     }
-
+    
     return 0;
 }
