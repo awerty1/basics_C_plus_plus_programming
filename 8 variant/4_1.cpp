a6cce81abd30c3ff4eb2a2c65447af857728a420
pheno <testuniversity@yandex.ru>
2024-01-09 20:31:56 +0300

    Update 4_1.cpp
    refactored

--- 8 variant/4_1.cpp
+++ 8 variant/4_1.cpp
@@ -11,11 +11,13 @@
 */
 
 #include <iostream>
+
 using namespace std;
 
 const int MAX_SIZE = 10;
 
-int main() {
+int main() 
+{
     int n;
     int matrix[MAX_SIZE][MAX_SIZE];
 
@@ -23,23 +25,30 @@ int main() {
     cin >> n;
 
     // заполнение матрицы
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j || i + j == n - 1) {
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            if (i == j || i + j == n - 1) 
+            {
                 matrix[i][j] = 0;
-            } else {
+            } 
+            else 
+            {
                 matrix[i][j] = 1;
             }
         }
     }
-
+    
     // вывод матрицы на экран
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
-
+    
     return 0;
 }
