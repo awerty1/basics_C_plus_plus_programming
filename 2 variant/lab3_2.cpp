b766e191f8697bb5bbc5c14d6c5a08190b56307f
pheno <testuniversity@yandex.ru>
2024-01-10 01:46:12 +0300

    Update lab3_2.cpp
    refactored

--- 2 variant/lab3_2.cpp
+++ 2 variant/lab3_2.cpp
@@ -11,33 +11,32 @@ using namespace std;
 //const int o=20;
 int main()
 { 
-setlocale(LC_ALL,"Russian");
- int n;
+    setlocale(LC_ALL,"Russian");
+    int n;
  
- int a[20], k = 0, m=1; 
- //srand(time(0)); 
- cout<<"Введите n \n";
- cin>>n;
- if (n<=20)
+    int a[20], k = 0, m=1; 
+    //srand(time(0)); 
+    cout<<"Введите n \n";
+    cin>>n;
+    if (n<=20)
     {
          for (int i = 0; i < n; i++) 
-            {
+         {
                 a[i] = rand() % 100;
                 cout << a[i] << "\t";
-                
+         }
+         for (int i = 0; i < n; i++) 
+         {
+            if ((a[i]==0)&&(a[i+m]==0)) 
+            {
+                m--;
+                k=k+1;
             }
-		  for (int i = 0; i < n; i++) 
-		  {
-			  if ((a[i]==0)&&(a[i+m]==0)) 
-                {
-			m--;
-                    k=k+1;
-					
-                }
-		  }
+         }
     }
- cout<<"\n"<<"Количество подряд идущих нулей= "<<k<<"\n";
- system ("pause");
+    cout<<"\n"<<"Количество подряд идущих нулей= "<<k<<"\n";
+    system ("pause");
+    
     return 0;
 
 }
