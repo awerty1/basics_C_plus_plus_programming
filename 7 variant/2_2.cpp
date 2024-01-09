9a722f8ee227bbe482da4d36bceab5d6554d186f
pheno <testuniversity@yandex.ru>
2024-01-09 20:44:49 +0300

    Update 2_2.cpp
    refactored

--- 7 variant/2_2.cpp
+++ 7 variant/2_2.cpp
@@ -9,17 +9,17 @@
 int main() 
 {
     std::cout << "Трехзначные числа, у которых вторая цифра кратна 3, а третья цифра кратна 4:" << std::endl;
-
+    
     for (int i = 100; i <= 999; ++i) 
-	{
+    {
         int secondDigit = (i / 10) % 10;
         int thirdDigit = i % 10;
-
+        
         if (secondDigit % 3 == 0 && secondDigit != 0 && thirdDigit != 0 && thirdDigit % 4 == 0)
-		{
+        {
             std::cout << i << std::endl;
         }
     }
-
+    
     return 0;
 }
