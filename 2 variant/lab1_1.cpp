c19f373e98b2cb3e6913a488a6880d0f80176b45
pheno <testuniversity@yandex.ru>
2024-01-10 01:38:20 +0300

    Update lab1_1.cpp
    refactored

--- 2 variant/lab1_1.cpp
+++ 2 variant/lab1_1.cpp
@@ -7,33 +7,34 @@
 #include <iostream>
 #include <cmath>
 
-float calculateDistance(float x1, float y1, float x2, float y2) {
-  float deltaX = x2 - x1;
-  float deltaY = y2 - y1;
-  return sqrt(pow(deltaX, 2) + pow(deltaY, 2));
+float calculateDistance(float x1, float y1, float x2, float y2) 
+{
+    float deltaX = x2 - x1;
+    float deltaY = y2 - y1;
+    return sqrt(pow(deltaX, 2) + pow(deltaY, 2));
 }
 
 int main() 
 {
-  setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian");
 
-  float x1, y1, x2, y2;
+    float x1, y1, x2, y2;
 
-  std::cout << "Введите координаты первой точки:\n";
-  std::cin >> x1 >> y1;
+    std::cout << "Введите координаты первой точки:\n";
+    std::cin >> x1 >> y1;
 
-  std::cout << "Введите координаты второй точки:\n";
-  std::cin >> x2 >> y2;
+    std::cout << "Введите координаты второй точки:\n";
+    std::cin >> x2 >> y2;
 
-  if (x1 == x2 && y1 == y2) 
-  {
-    std::cout << "Координаты точек совпадают.\n";
-  } 
-  else 
-  {
-    float distance = calculateDistance(x1, y1, x2, y2);
-    std::cout << "Расстояние между двумя точками: " << distance << std::endl;
-  }
+    if (x1 == x2 && y1 == y2) 
+    {
+        std::cout << "Координаты точек совпадают.\n";
+    } 
+    else 
+    {
+        float distance = calculateDistance(x1, y1, x2, y2);
+        std::cout << "Расстояние между двумя точками: " << distance << std::endl;
+    }
 
-  return 0;
+    return 0;
 }
