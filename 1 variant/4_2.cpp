d8e214000afbaf9cce4fd484644eb4844363c098
pheno <testuniversity@yandex.ru>
2024-01-10 01:06:38 +0300

    Update 4_2.cpp
    refactored

--- 1 variant/4_2.cpp
+++ 1 variant/4_2.cpp
@@ -22,56 +22,56 @@ int main()
     int** matrix = new int*[n];
     srand(time(0));
     for (int i = 0; i < n; i++) 
-	{
-		matrix[i] = new int[m];
+    {
+        matrix[i] = new int[m];
         for (int j = 0; j < m; j++) 
-		{
+        {
             matrix[i][j] = rand() % 10 + 1;
         }
     }
-
+    
     // Вывод матрицы
     std::cout << "Матрица:\n";
     for (int i = 0; i < n; i++) 
-	{
+    {
         for (int j = 0; j < m; j++) 
-		{
+        {
             std::cout << matrix[i][j] << "\t";
         }
         std::cout << "\n";
     }
-
+    
     // Поиск наименьшего элемента и суммы строки
     int minElement = matrix[0][0];
     int minRowSum = 0;
     int minRowIndex = 0;
     for (int i = 0; i < n; i++) 
-	{
+    {
         int rowSum = 0;
         for (int j = 0; j < m; j++) 
-		{
+        {
             if (matrix[i][j] < minElement) 
-			{
+            {
                 minElement = matrix[i][j];
                 minRowIndex = i;
             }
             rowSum += matrix[i][j];
         }
         if (i == minRowIndex) 
-		{
+        {
             minRowSum = rowSum;
         }
     }
-
+    
     // Вывод наименьшего элемента и суммы строки
     std::cout << "Наименьший элемент матрицы: " << minElement << "\n";
     std::cout << "Сумма строки с наименьшим элементом: " << minRowSum << "\n";
-	
-	for (int i = 0; i < n; i++) 
-	{
-		delete[] matrix[i];
-	}
-	delete[] matrix;
-
+    
+    for (int i = 0; i < n; i++) 
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+    
     return 0;
 }
