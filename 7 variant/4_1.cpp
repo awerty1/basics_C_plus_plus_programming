1a278f1c2b4312c50199f488d3f4c326be02f443
pheno <testuniversity@yandex.ru>
2024-01-11 00:32:09 +0300

    Update 4_1.cpp
    refactored

--- 7 variant/4_1.cpp
+++ 7 variant/4_1.cpp
@@ -23,19 +23,19 @@ std::vector<std::vector<int>> generateMatrix(int n)
     {
         throw std::invalid_argument("Размер матрицы должен быть положительным");
     }
-
+    
     std::vector<std::vector<int>> matrix(n, std::vector<int>(n, 0));
-
+    
     for (int i = 0; i < n; i++) 
     {
         matrix[i][i] = 1;  // Заполняем главную диагональ единицами
-
+        
         if (i != n / 2) 
         {
             matrix[i][n - i - 1] = 2;  // Заполняем побочную диагональ (за исключением центрального элемента) двойками
         }
     }
-
+    
     return matrix;
 }
 
@@ -58,16 +58,16 @@ int main()
         int n;
         std::cout << "Введите размер матрицы (n): ";
         std::cin >> n;
-
+        
         std::vector<std::vector<int>> matrix = generateMatrix(n);
-
+        
         std::cout << "Сформированная матрица:" << std::endl;
         printMatrix(matrix);
     } 
-	catch (const std::exception& ex) 
-	{
+    catch (const std::exception& ex) 
+    {
         std::cout << "Ошибка: " << ex.what() << std::endl;
     }
-
+    
     return 0;
 }
