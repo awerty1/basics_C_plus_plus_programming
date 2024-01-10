4a44af3610c74bd581ae54fcf7389c077299ec12
pheno <testuniversity@yandex.ru>
2024-01-11 00:10:29 +0300

    Update exam2.cpp
    updated

--- exam/3 semester/exam2.cpp
+++ exam/3 semester/exam2.cpp
@@ -8,13 +8,13 @@ int findDivisors(int n)
 {
     int count = 0;
     for (int i = 1; i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			count++;
-		}
-	}
-		
+    {
+        if (n % i == 0)
+        {
+            count++;
+        }
+    }
+    
     return count;
 }
 
