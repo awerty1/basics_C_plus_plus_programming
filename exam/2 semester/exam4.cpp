f4c0fdf441e4631f79eac9105e3c9972b68681db
pheno <testuniversity@yandex.ru>
2024-01-12 00:01:06 +0300

    Update exam4.cpp
    updated

--- exam/2 semester/exam4.cpp
+++ exam/2 semester/exam4.cpp
@@ -9,30 +9,31 @@
 
 const int MAX_SIZE = 100;
 
-int main() {
+int main() 
+{
     int n, maxElementRow, maxElementCol;
     double matrix[MAX_SIZE][MAX_SIZE]; 
-	double maxElementValue = 0;
-
+    double maxElementValue = 0;
+    
     std::cout << "Enter the size of the square matrix: ";
     std::cin >> n;
-
+    
     // Input matrix elements
     std::cout << "Enter the elements of the matrix:" << std::endl;
     for (int i = 0; i < n; i++) 
-	{
+    {
         for (int j = 0; j < n; j++) 
-		{
+        {
             std::cin >> matrix[i][j];
             if (std::abs(matrix[i][j]) > maxElementValue) 
-			{
+            {
                 maxElementValue = std::abs(matrix[i][j]);
                 maxElementRow = i;
                 maxElementCol = j;
             }
         }
     }
-
+    
     // Find the element with the maximum absolute value
     std::cout << "The maximum absolute value is " << maxElementValue << std::endl;
     std::cout << "It is located at row " << maxElementRow + 1 << " and column " << maxElementCol + 1 << std::endl;
@@ -41,16 +42,16 @@ int main() {
     double newMatrix[MAX_SIZE-1][MAX_SIZE-1];
     int newRow = 0;
     for (int i = 0; i < n; i++) 
-	{
+    {
         if (i == maxElementRow) 
-		{
+        {
             continue;
         }
         int newCol = 0;
         for (int j = 0; j < n; j++) 
-		{
+        {
             if (j == maxElementCol) 
-			{
+            {
                 continue;
             }
             newMatrix[newRow][newCol] = matrix[i][j];
@@ -58,17 +59,17 @@ int main() {
         }
         newRow++;
     }
-
+    
     // Output the new matrix
     std::cout << "The new matrix is:" << std::endl;
     for (int i = 0; i < n-1; i++) 
-	{
+    {
         for (int j = 0; j < n-1; j++) 
-		{
+        {
             std::cout << newMatrix[i][j] << " ";
         }
         std::cout << std::endl;
     }
-
+    
     return 0;
 }
