8c3d0b0848cd2de5b71ce32693efdd794cace16c
pheno <testuniversity@yandex.ru>
2024-01-12 00:03:09 +0300

    Update 1_3.cpp
    updated

--- 7 variant/1_3.cpp
+++ 7 variant/1_3.cpp
@@ -4,21 +4,39 @@
 */
 
 #include <iostream>
+#include <limits>
 
 int main() 
 {
     int x, y, z;
     float average;
-
-    // Ввод чисел x, y и z
-    std::cout << "Введите три числа: ";
-    std::cin >> x >> y >> z;
-
-    // Находим среднее значение
-    average = (float)(x + y + z) / 3;
-
-    // Выводим среднее значение на экран
-    std::cout << "Среднее значение: " << average << std::endl;
-
+    
+    do
+    {
+        // Ввод чисел x, y и z
+        std::cout << "Введите три числа: ";
+    
+        // Проверка на корректный ввод
+        if (!(std::cin >> x >> y >> z)) 
+        {
+            std::cout << "Ошибка ввода. Пожалуйста, введите только числа." << std::endl;
+            // Очистка флага ошибки ввода
+            std::cin.clear();
+            // Очистка буфера ввода
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            
+        }
+        else
+        {
+            // Находим среднее значение
+            average = static_cast<float>(x + y + z) / 3;
+        
+            // Выводим среднее значение на экран
+            std::cout << "Среднее значение: " << average << std::endl;
+            break; // Выход из цикла после корректного ввода
+        }
+    }
+    while(true);
+    
     return 0;
 }
