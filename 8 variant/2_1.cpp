e66f02e3018e7ef95fef4f88028d66da0656dac7
pheno <testuniversity@yandex.ru>
2024-01-14 02:18:39 +0300

    Update 2_1.cpp
    updated

--- 8 variant/2_1.cpp
+++ 8 variant/2_1.cpp
@@ -1,41 +1,53 @@
 /*
-Задание 1. С клавиатуры вводятся десять целых чисел. Определить, сколько 
-среди них положительных, отрицательных и равных нулю. 
+Задание 1. С клавиатуры вводятся десять целых чисел. Определить, сколько
+среди них положительных, отрицательных и равных нулю.
 */
 
 #include <iostream>
+#include <limits>
 
-int main() 
+int main()
 {
     int num;
     int positiveCount = 0;
     int negativeCount = 0;
     int zeroCount = 0;
-
+    
     std::cout << "Введите 10 целых чисел:\n";
-
-    for (int i = 0; i < 10; ++i) 
+    
+    for (int i = 0; i < 10; ++i)
     {
-        std::cout << "Число " << i+1 << ":"; 
-        std::cin >> num;
-
-        if (num > 0) 
+        std::cout << "Число " << i+1 << ":";
+        //std::cin >> num;
+        if (!(std::cin >> num))
+        {
+            std::cout << "Ошибка ввода символов!" << std::endl;
+            
+            // Очищаем входной буфер
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            
+            // Уменьшаем счетчик, чтобы можно было повторно ввести значение
+            i--;
+        }
+        
+        if (num > 0)
         {
             positiveCount++;
-        } 
-        else if (num < 0) 
+        }
+        else if (num < 0)
         {
             negativeCount++;
-        } 
-        else 
+        }
+        else
         {
             zeroCount++;
         }
     }
-
+    
     std::cout << "Количество положительных чисел: " << positiveCount << "\n";
     std::cout << "Количество отрицательных чисел: " << negativeCount << "\n";
     std::cout << "Количество нулевых чисел: " << zeroCount << "\n";
-
+    
     return 0;
 }
