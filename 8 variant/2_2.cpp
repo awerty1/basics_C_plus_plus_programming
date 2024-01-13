698fb0127f83c614b5072fe151a6a1a19b1685e2
pheno <testuniversity@yandex.ru>
2024-01-14 02:11:38 +0300

    Update 2_2.cpp
    updated

--- 8 variant/2_2.cpp
+++ 8 variant/2_2.cpp
@@ -1,17 +1,18 @@
 /*
-Задание 2. C клавиатуры вводится число n. Найти число из диапазона от 1 
-до n с максимальной суммой делителей (в делители не включаются 1 и само 
+Задание 2. C клавиатуры вводится число n. Найти число из диапазона от 1
+до n с максимальной суммой делителей (в делители не включаются 1 и само
 число). Сделать проверку на корректность ввода n.
 */
 
 #include <iostream>
+#include <limits>
 
-int getSumOfDivisors(int number) 
+int getSumOfDivisors(int number)
 {
     int sum = 0;
-    for (int i = 2; i <= number / 2; ++i) 
+    for (int i = 2; i <= number / 2; ++i)
     {
-        if (number % i == 0) 
+        if (number % i == 0)
         {
             sum += i;
         }
@@ -19,34 +20,44 @@ int getSumOfDivisors(int number)
     return sum;
 }
 
-int main() 
+int main()
 {
     int n;
-
-    std::cout << "Введите число n: ";
-    std::cin >> n;
-
-    // Проверка на корректность ввода
-    if (n <= 0) 
+    
+    do
     {
-        std::cout << "Число должно быть положительным.\n";
-        return 0;
+        std::cout << "Введите число n: ";
+        
+        //Проверка корректности ввода n
+        if (!(std::cin >> n))
+        {
+            std::cout << "Ошибка ввода!" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        // Проверка на корректность ввода
+        else if (n <= 0)
+        {
+            std::cout << "Число должно быть положительным.\n";
+        }
     }
-
+    while (n <= 0);
+    
+    
     int maxNumber = 1;
     int maxSum = 0;
-
-    for (int i = 2; i <= n; ++i) 
+    
+    for (int i = 2; i <= n; ++i)
     {
         int sum = getSumOfDivisors(i);
-        if (sum > maxSum) 
+        if (sum > maxSum)
         {
             maxSum = sum;
             maxNumber = i;
         }
     }
-
+    
     std::cout << "Число из диапазона от 1 до " << n << " с максимальной суммой делителей: " << maxNumber << "\n";
-
+    
     return 0;
 }
