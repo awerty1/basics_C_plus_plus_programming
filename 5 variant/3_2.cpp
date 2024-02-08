0ec7bf55768ce5744cb6229510599a7c9f3e9d1f
pheno <testuniversity@yandex.ru>
2024-02-09 00:50:00 +0300

    Update 3_2.cpp
    updated
    
    Co-Authored-By: ivaneh98 <50576867+ivaneh98@users.noreply.github.com>
    Co-Authored-By: TemplarD <85034279+TemplarD@users.noreply.github.com>

--- 5 variant/3_2.cpp
+++ 5 variant/3_2.cpp
@@ -9,59 +9,110 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <limits>
+#include <sstream>
 
-int main() 
+int getValidInput(const std::string &message)
+{
+    int input;
+    bool validInput = false;
+    std::string inputStr;
+    
+    do
+    {
+        std::cout << message;
+        std::cin >> inputStr;
+        
+        bool validNumber = true;
+        for (char c : inputStr)
+        {
+            if (!std::isdigit(c))
+            {
+                validNumber = false;
+                break;
+            }
+        }
+        
+        if (validNumber)
+        {
+            std::stringstream ss(inputStr);
+            ss >> input;
+            if (input > 0 && input <= 20)
+            {
+                validInput = true;
+            }
+            else
+            {
+                std::cerr << "Ошибка: значение должно быть в диапазоне (1, 20). Попробуйте снова." << std::endl;
+            }
+        }
+        else
+        {
+            std::cerr << "Ошибка: некорректный ввод. Попробуйте снова." << std::endl;
+        }
+        
+        if (!validInput)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        
+    } while (!validInput);
+    
+    return input;
+}
+
+int main()
 {
     int n;
-
-    do 
+    
+    do
     {
-        std::cout << "Введите значение n (не больше 20): ";
-        std::cin >> n;
-
-        if (n <= 0 || n > 20) 
+        n = getValidInput("Введите значение n (не больше 20): ");
+        
+        if (n <= 0 || n > 20)
         {
             std::cerr << "Ошибка: некорректный ввод n. Повторите попытку." << std::endl;
         }
-    } 
+    }
     while (n <= 0 || n > 20);
-
+    
     std::vector<int> arr(n);
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(-10, 10);
-
-    for (int i = 0; i < n; ++i) 
+    
+    for (int i = 0; i < n; ++i)
     {
         arr[i] = dis(gen);
     }
-
+    
     std::cout << "Сформированный массив: ";
-    for (int num : arr) 
+    for (int num : arr)
     {
         std::cout << num << " ";
     }
     std::cout << std::endl;
-
+    
     int max_length = 0;
     int current_length = 0;
-
-    for (int num : arr) 
+    
+    for (int num : arr)
     {
-        if (num > 0) 
+        if (num > 0)
         {
             current_length++;
-        } 
-        else 
+        }
+        else
         {
             max_length = std::max(max_length, current_length);
             current_length = 0;
         }
     }
-
+    
     max_length = std::max(max_length, current_length);
-
+    
     std::cout << "Максимальное количество последовательных положительных элементов: " << max_length << std::endl;
-
+    
     return 0;
 }
